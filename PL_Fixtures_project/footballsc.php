7c9aae9b106696510b46631c55c5647ee036d5ce
dongchime <daleyblind17@kookmin.ac.kr>
2017-01-12 01:28:08 +0900

    made ui 1

---
+++ PL_Fixtures_project/footballsc.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type=text/css href="stylefs.css">
+  </head>
+  <body>
+    <header>
+      <img src="http://footballfanfc.com/wp-content/uploads/2015/02/EPL.png" alt="EPL">
+      <h1><a href="http://localhost/MyProject/footballsc.php">Premier League Fixtures</a></h1>
+    </header>
+    <nav>
+      <ol>
+        <li><a href="http://localhost/MyProject/page_mu.html"><img id="manu" src="http://vignette2.wikia.nocookie.net/fifa/images/2/26/Manchester_United_logo.png/revision/latest?cb=20120420192747" alt="ManU"</a></li>
+        <li><a href="http://localhost/MyProject/page_muc.html"><img id="mancity" src="http://vignette4.wikia.nocookie.net/logopedia/images/3/36/Manchester_City_2016.png/revision/latest?cb=20151229013856" alt="ManCity"></a></li>
+        <li><a href="http://localhost/MyProject/page_che.html"><img id="chelsea" src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Chelsea_FC.svg/1024px-Chelsea_FC.svg.png" alt="chelsea"></a></li>
+        <li><a href="http://localhost/MyProject/page_ars.html"><img id="arsenal" src="http://vignette4.wikia.nocookie.net/fifa/images/9/92/Arsenal-logo.png/revision/latest?cb=20111224200103" alt="Arsenal"></a></li>
+        <li><a href="http://localhost/MyProject/page_tot.html"><img id="tot" src="http://soccerlogo.net/uploads/posts/2014-09/1410464191_fc-tottenham-hotspur.png" alt="Tottenham Hotspur"></a></li>
+        <li><a href="http://localhost/MyProject/page_liv.html"><img id="liv" src="http://vignette4.wikia.nocookie.net/pes/images/1/1b/Liverpool-logo.png/revision/latest?cb=20150617145940" alt="Liverpool"></a></li>
+      </ol>
+    </nav>
+  </body>
+</html>
